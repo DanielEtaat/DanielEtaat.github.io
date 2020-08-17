60edca5c3c435e07b798b00f3ea2c7e9382e57dd
Daniel Etaat <34327451+DanielEtat@users.noreply.github.com>
2020-08-16 21:35:49 -0700

    add weekly goals

--- js/index.js
+++ js/index.js
@@ -1,8 +1,29 @@
 window.onload = function() {
+
+  // Selecting the correct profile based on the url params
   const queryString = window.location.search;
   const urlParams = new URLSearchParams(queryString);
   if (urlParams.get("user") !== null) {
     document.getElementById("user").value = urlParams.get("user");
   }
+
+  // Updating weekly and daily goals on the profile page
+  var weekGoal = getWeeklyGoal();
+  var dayGoal = Math.floor(weekGoal/7);
+  document.getElementById("weekgoal").innerText = weekGoal.toString();
+  document.getElementById("daygoal").innerText = dayGoal.toString();
+
+  // Retrieving user data
   retrieveTotals();
+}
+
+
+// Returns the target number of pushups to be completed each week
+function getWeeklyGoal() {
+  const startDate = 1597042800000;
+  const today = new Date();
+  var daysPast = (today.getTime()-startDate) / 86400000;
+  var weekNumber = Math.floor(daysPast / 7);
+  var goal = 1000 + weekNumber * 250;
+  return goal;
 }
